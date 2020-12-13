f0c2f01ea2a9c08d2f44e17ea974d959098b8e66
leobuck <leobuck12@gmail.com>
2020-12-13 19:17:29 -0300

    Ajuste datasource

--- src/main/java/com/curso/springboot/SpringbootApplication.java
+++ src/main/java/com/curso/springboot/SpringbootApplication.java
@@ -1,21 +1,17 @@
 package com.curso.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.Ordered;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 //@EntityScan(basePackages = "com.curso.springboot.model.*")
 @ComponentScan(basePackages = {"com.curso.*"})
 //@EnableJpaRepositories(basePackages = {"com.curso.springboot.repository.*"})
 @EnableTransactionManagement
+@EnableAutoConfiguration
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
